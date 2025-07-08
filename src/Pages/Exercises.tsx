671b9d470c014273828b91955c16f62c24c6e587
miho19 <miho19@hotmail.co.nz>
2025-07-09 04:17:49 +1200

    Exercise page displays a list of exercises

--- src/Pages/Exercises.tsx
+++ src/Pages/Exercises.tsx
@@ -2,16 +2,16 @@ import ExerciseContainer from "../Components/Exercise/ExerciseContainer";
 import type { ExerciseListElement } from "../Components/Exercise/ExerciseTypes";
 import HomeButton from "../Components/Navigation/HomeButton";
 
-const exerciseList: ExerciseListElement[] = [
-  {
-    name: "Incline Hammer Curls",
-    img: "https://static.strengthlevel.com/images/exercises/incline-hammer-curl/incline-hammer-curl-800.jpg",
-    link: "/exercise/1",
-    id: "12345",
-  },
-];
-
 function ExercisePage() {
+  const exerciseList: ExerciseListElement[] = [
+    {
+      name: "Incline Hammer Curls",
+      img: "https://static.strengthlevel.com/images/exercises/incline-hammer-curl/incline-hammer-curl-800.jpg",
+      link: "/exercise/1",
+      id: "12345",
+    },
+  ];
+
   return (
     <main>
       <h1>Exericses</h1>
