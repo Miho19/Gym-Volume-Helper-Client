f6f28412608e6e60062d506d3c40b6f355b861b2
miho19 <miho19@hotmail.co.nz>
2025-07-09 17:27:46 +1200

    Fetch User function

--- src/Hooks/useUserQuery.ts
+++ src/Hooks/useUserQuery.ts
@@ -6,21 +6,49 @@ type UserResponseType = {
   weight: string;
 };
 
-function fetchUser(userSub: string): Promise<UserResponseType> {
+async function fetchUser(
+  userSub: string | undefined
+): Promise<UserResponseType> {
   if (!userSub || userSub === undefined)
     throw new Error("Missing user Sub argument");
 
-  return new Promise(() => {});
+  const fetchOptions: RequestInit = {
+    mode: "cors",
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ auth0ID: userSub }),
+    credentials: "include",
+  };
+
+  const URL = import.meta.env.DEV ? "http://localhost:5052/auth/" : "";
+
+  try {
+    const response: Response = await fetch(URL, fetchOptions);
+
+    if (!response.ok) throw new Error("Failed to fetch user");
+
+    const body: unknown = await response.json();
+
+    // validate response using ZOD
+
+    return body as UserResponseType;
+  } catch (error) {
+    if (error instanceof Error) console.log(error);
+    throw new Error("Failed to authenicate user");
+  }
 }
 
 type Props = {};
 
-function useUserQuery(_props: Props) {
+export default function useUserQuery(_props: Props) {
   const { user, isAuthenticated } = useAuth0();
 
   return useQuery({
     queryKey: ["auth0 User"],
-    queryFn: () => "ll",
+    queryFn: () => fetchUser(user?.sub),
     enabled: isAuthenticated,
   });
 }
