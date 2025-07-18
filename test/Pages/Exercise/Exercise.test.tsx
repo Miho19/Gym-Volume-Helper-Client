2b5c1aa9180700f6acb0d89a12fea92fd899f103
miho19 <miho19@hotmail.co.nz>
2025-07-18 14:24:41 +1200

    Cleaned up some imports

--- test/Pages/Exercise/Exercise.test.tsx
+++ test/Pages/Exercise/Exercise.test.tsx
@@ -1,18 +1,14 @@
 import React from "react";
-import { render, screen, waitFor } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 import { describe, it, expect, vi, beforeEach } from "vitest";
-import { router } from "../../../src/Router/Router";
 import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom";
-import { RouterProvider } from "react-router";
 import * as auth0 from "@auth0/auth0-react";
 import { testUser } from "../../../src/Components/ProfileBubble/UserType";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import { testRender } from "../../Util";
 
 vi.mock("@auth0/auth0-react");
-
-const client = new QueryClient();
 
 describe("Exercise Page", async () => {
   it("render exercise page", async () => {
