653f997a9747ed172755da7386375998af93a88c
miho19 <miho19@hotmail.co.nz>
2025-07-24 18:04:54 +1200

    Added label/input with state for adding new metric

--- src/Components/Exercise/Individual/ExerciseMetricsAddNew.tsx
+++ src/Components/Exercise/Individual/ExerciseMetricsAddNew.tsx
@@ -1,9 +1,49 @@
+import { useState } from "react";
+
 type Props = {
   exerciseID: string;
 };
 
+type FormData = {
+  weight: number;
+  reps: number;
+};
+
 function ExerciseMetricsNew(props: Props) {
-  return <section>Add new Metric</section>;
+  const [metricData, setMetricData] = useState<FormData>({
+    weight: 0,
+    reps: 0,
+  });
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const { name, value } = event.target;
+    setMetricData((previous) => ({ ...previous, [name]: value }));
+  }
+
+  return (
+    <section>
+      <p>Add New</p>
+      <label>
+        weight:
+        <input
+          type="number"
+          name="weight"
+          value={metricData.weight}
+          onChange={handleChange}
+        />
+      </label>
+
+      <label>
+        reps:
+        <input
+          type="number"
+          name="reps"
+          value={metricData.reps}
+          onChange={handleChange}
+        />
+      </label>
+    </section>
+  );
 }
 
 export default ExerciseMetricsNew;
