5d1f656ed58fcc0d8f6dfa810f4a35239a2f2351
miho19 <miho19@hotmail.co.nz>
2025-08-21 23:09:16 +1200

    Handling of adding exercise to list for new workout preset

--- src/Components/WorkoutPreset/NewWorkoutPreset/NewWorkoutPresetForm.tsx
+++ src/Components/WorkoutPreset/NewWorkoutPreset/NewWorkoutPresetForm.tsx
@@ -1,5 +1,65 @@
+import { useNavigate } from "react-router";
+import NewWorkoutPresetFormExerciseAdder from "./NewWorkoutPresetFormExerciseAdder";
+import { useState } from "react";
+
+export type NewWorkoutPresetFormDataType = {
+  workoutName: string;
+  workoutPicture: string;
+  exerciseNameList: string[];
+};
+
+const initialFormState: NewWorkoutPresetFormDataType = {
+  workoutName: "",
+  workoutPicture: "",
+  exerciseNameList: [],
+};
+
 function NewWorkoutPresetForm() {
-  return <form></form>;
+  const navigation = useNavigate();
+
+  const [formData, setFormData] = useState(initialFormState);
+
+  function handleOnChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const { name, value } = event.target;
+    setFormData((previous) => ({ ...previous, [name]: value }));
+  }
+
+  return (
+    <>
+      <button onClick={() => navigation("/workout")}>Back</button>
+      <form style={{ display: "flex", flexDirection: "column", gap: "5px" }}>
+        <label>
+          Workout Name
+          <input
+            type="text"
+            maxLength={25}
+            min={3}
+            name="workoutName"
+            value={formData.workoutName}
+            onChange={handleOnChange}
+          />
+        </label>
+
+        <label>
+          Workout Picture
+          <input
+            type="text"
+            maxLength={25}
+            min={3}
+            name="workoutPicture"
+            value={formData.workoutPicture}
+            onChange={handleOnChange}
+          />
+        </label>
+
+        <NewWorkoutPresetFormExerciseAdder setFormData={setFormData} />
+        <textarea
+          name="exerciseNameList"
+          value={formData.exerciseNameList.join("\n")}
+        />
+      </form>
+    </>
+  );
 }
 
 export default NewWorkoutPresetForm;
---
+++ src/Components/WorkoutPreset/NewWorkoutPreset/NewWorkoutPresetFormExerciseAdder.tsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import type { NewWorkoutPresetFormDataType } from "./NewWorkoutPresetForm";
+
+type Props = {
+  setFormData: React.Dispatch<
+    React.SetStateAction<NewWorkoutPresetFormDataType>
+  >;
+};
+
+function NewWorkoutPresetFormExerciseAdder(props: Props) {
+  const [exerciseName, setExerciseName] = useState<string>("");
+
+  function handleOnClick(event: React.MouseEvent<HTMLButtonElement>) {
+    event.preventDefault();
+
+    const { setFormData } = props;
+
+    setFormData((previous: NewWorkoutPresetFormDataType) => {
+      const previousExerciseList = previous.exerciseNameList;
+
+      if (
+        previousExerciseList.findIndex(
+          (exercise) => exercise === exerciseName
+        ) !== -1
+      )
+        return previous;
+
+      previousExerciseList.push(exerciseName);
+
+      setExerciseName("");
+
+      return { ...previous, exerciseNameList: previousExerciseList };
+    });
+  }
+
+  function handleOnChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const { value } = event.target;
+    setExerciseName(value);
+  }
+
+  return (
+    <>
+      <label>
+        Exercise Name
+        <input
+          type="text"
+          name="exerciseName"
+          value={exerciseName}
+          onChange={handleOnChange}
+        />
+        <button onClick={handleOnClick}>Add</button>
+      </label>
+    </>
+  );
+}
+
+export default NewWorkoutPresetFormExerciseAdder;
