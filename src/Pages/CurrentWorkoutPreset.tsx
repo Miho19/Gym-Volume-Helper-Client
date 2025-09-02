f31a3654eecb328c70759951d152e96035df1c19
miho19 <miho19@hotmail.co.nz>
2025-09-03 01:31:39 +1200

    trialling not using dependent queries to get users current workout then workout preset

--- src/Pages/CurrentWorkoutPreset.tsx
+++ src/Pages/CurrentWorkoutPreset.tsx
@@ -1,4 +1,31 @@
+import useInitialiseUserQuery from "../Hooks/useInitialiseUserQuery";
+import useWorkoutPresetQuery from "../Hooks/useWorkoutPresetQuery";
+
 function CurrentWorkoutPresetPage() {
+  const {
+    data: user,
+    isLoading: isLoadingUser,
+    isError: isErrorUser,
+    error: errorUser,
+  } = useInitialiseUserQuery();
+
+  if (isLoadingUser) return <div>Loading...</div>;
+  if (isErrorUser) return <div>{errorUser?.message}</div>;
+
+  const { currentWorkoutID } = user!;
+
+  const {
+    data: workout,
+    isLoading: isLoadingWorkout,
+    isError: isErrorWorkout,
+    error: errorWorkout,
+  } = useWorkoutPresetQuery({
+    workoutID: currentWorkoutID,
+  });
+
+  if (isLoadingWorkout) return <div>Loading...</div>;
+  if (isErrorWorkout) return <div>{errorWorkout?.message}</div>;
+
   return <main></main>;
 }
 
