bb951d87224450d4c07ad05b517afa075dcbb332
miho19 <miho19@hotmail.co.nz>
2025-09-17 02:25:20 +1200

    slight adjustment to scaling of home button on hover and applying margin only to y axis

--- src/Components/WorkoutPreset/IndivdualWorkoutPreset/WorkoutPresetOwnerPanel.tsx
+++ src/Components/WorkoutPreset/IndivdualWorkoutPreset/WorkoutPresetOwnerPanel.tsx
@@ -8,9 +8,9 @@ function WorkoutPresetOwnerPanel(props: Props) {
   const { workout } = props;
 
   return (
-    <section className="w-full h-full border border-white/5 bg-[#31363F] p-3 flex flex-row items-center gap-x-5 rounded-lg">
+    <section className="w-full border border-white/5 bg-[#31363F] p-3 flex flex-row items-center gap-x-5 rounded-lg">
       <img
-        className="w-10 h-10 border border-white/5 rounded-full"
+        className="w-10 h-10 border border-white/5 rounded-full object-fill"
         src={workout.ownerProfilePictureURL}
         alt={`profile picture of ${workout.ownerName}`}
       />
