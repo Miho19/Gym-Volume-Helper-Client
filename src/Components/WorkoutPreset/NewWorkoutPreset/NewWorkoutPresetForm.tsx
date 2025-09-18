eac1efecb08381a337a266295b1169f01ea52489
miho19 <miho19@hotmail.co.nz>
2025-09-18 23:04:08 +1200

    Add new workout preset styling

--- src/Components/WorkoutPreset/NewWorkoutPreset/NewWorkoutPresetForm.tsx
+++ src/Components/WorkoutPreset/NewWorkoutPreset/NewWorkoutPresetForm.tsx
@@ -33,14 +33,13 @@ function NewWorkoutPresetForm() {
   }
 
   return (
-    <>
-      <button onClick={() => navigation("/workout")}>Back</button>
-      <form
-        style={{ display: "flex", flexDirection: "column", gap: "5px" }}
-        onSubmit={handleOnSubmit}
-      >
-        <label>
-          Workout Name
+    <form
+      className="w-full h-full flex flex-col gap-y-5"
+      onSubmit={handleOnSubmit}
+    >
+      <section className="w-full h-full flex flex-col border border-white/5 bg-[#31363F] rounded-lg p-3 gap-y-5">
+        <label className="w-full h-full grid grid-cols-5">
+          <span className="col-span-2">Workout Name</span>
           <input
             type="text"
             maxLength={25}
@@ -48,11 +47,12 @@ function NewWorkoutPresetForm() {
             name="workoutName"
             value={formData.workoutName}
             onChange={handleOnChange}
+            className="w-full border border-white/5 col-span-3 rounded-lg px-2"
           />
         </label>
 
-        <label>
-          Workout Picture
+        <label className="w-full h-full grid grid-cols-5">
+          <span className="col-span-2">Workout Picture</span>
           <input
             type="text"
             maxLength={25}
@@ -60,15 +60,27 @@ function NewWorkoutPresetForm() {
             name="workoutPicture"
             value={formData.workoutPicture}
             onChange={handleOnChange}
+            className="w-full border border-white/5 col-span-3 rounded-lg px-2"
           />
         </label>
+      </section>
 
-        <button type="submit">Create Workout Preset</button>
-        <button type="reset" onClick={resetForm}>
+      <section className="flex flex-col gap-y-5">
+        <button
+          type="submit"
+          className="border border-white/5 rounded-full w-full h-10 cursor-pointer text-center transition-all duration-300 hover:scale-101 hover:font-bold bg-[#31363F] flex flex-col justify-center items-center"
+        >
+          Create Workout Preset
+        </button>
+        <button
+          type="reset"
+          className="border border-white/5 rounded-full w-full h-10 cursor-pointer text-center transition-all duration-300 hover:scale-101 hover:font-bold bg-[#31363F] flex flex-col justify-center items-center"
+          onClick={resetForm}
+        >
           Reset
         </button>
-      </form>
-    </>
+      </section>
+    </form>
   );
 }
 
--- src/Pages/NewWorkoutPreset.tsx
+++ src/Pages/NewWorkoutPreset.tsx
@@ -1,8 +1,18 @@
+import { useNavigate } from "react-router";
 import NewWorkoutPresetForm from "../Components/WorkoutPreset/NewWorkoutPreset/NewWorkoutPresetForm";
 
 function NewWorkoutPresetPage() {
+  const navigation = useNavigate();
+
   return (
-    <main>
+    <main className="mx-auto flex flex-col items-center gap-y-5 p-5 min-h-screen">
+      <button
+        onClick={() => navigation("/workout")}
+        className="border border-white/5 rounded-full w-full h-10 cursor-pointer text-center transition-all duration-300 hover:scale-101 hover:font-bold bg-[#31363F] flex flex-col justify-center items-center"
+      >
+        Back
+      </button>
+
       <NewWorkoutPresetForm />
     </main>
   );
