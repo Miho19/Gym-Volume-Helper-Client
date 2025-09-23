429d6a2ee391e98454df1da09a6c139ab9f2b67e
miho19 <miho19@hotmail.co.nz>
2025-09-23 23:37:11 +1200

    Styling for login finished, very basic with login button somewhat to resemble facebook button

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -1,20 +1,22 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import { useNavigate } from "react-router";
 
 function LoginPage() {
   const { loginWithRedirect } = useAuth0();
 
-  const navigator = useNavigate();
-
   return (
-    <main>
-      <h1>Gym Helper</h1>
-      <p>Explain what app does</p>
+    <main className="mx-auto flex flex-col items-center justify-center gap-y-5 p-5 min-h-screen">
+      <h1 className="text-8xl tracking-tighter font-semibold mb-5">
+        Gym Helper
+      </h1>
+      <p className="text-2xl">
+        Create workout plans to track progressive overload
+      </p>
 
       <button
         onClick={() => {
           loginWithRedirect();
         }}
+        className="border border-white/5 rounded-2xl py-5 px-10 bg-blue-500 font-semibold cursor-pointer hover:bg-blue-600 transition-all duration-300"
       >
         Log in With Facebook
       </button>
